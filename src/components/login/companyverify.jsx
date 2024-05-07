7b1a4d71adf32e6dae7c86f2963b5b172aa8f844
AswinC001 <97738277+AswinC001@users.noreply.github.com>
2024-05-07 21:54:58 +0530

    updated required for some fields in signup

--- src/components/login/companyverify.jsx
+++ src/components/login/companyverify.jsx
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-// import './companydash.css';
+import './companydash.css';
 import { v4 as uuidv4 } from 'uuid';
 import Web3 from "web3";
 import { Address } from "../../contractinfo/address";
